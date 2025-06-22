cdb08017b3bc984fee2fddc378b5b29320cc3cc3
Federico Beltramino <frbeltra2@gmail.com>
2025-06-22 14:57:55 -0300

    fix inicio idioma

--- src/components/TranslateDropdown.jsx
+++ src/components/TranslateDropdown.jsx
@@ -1,16 +1,21 @@
 import { Button, ButtonGroup } from "@mui/material"
 import { useTranslation } from "react-i18next"
+import { useEffect, useState } from "react"
 
 export const TranslateDropdown = () => {
-
-   const { i18n } = useTranslation()
+  const { i18n } = useTranslation()
+  const [currentLang, setCurrentLang] = useState(i18n.language)
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng)
+    setCurrentLang(lng)
   }
 
-  const currentLang = i18n.language
-
+  useEffect(() => {
+    if (!currentLang && i18n.language) {
+      setCurrentLang(i18n.language)
+    }
+  }, [i18n.language, currentLang])
 
   return (
     <ButtonGroup variant="contained">
